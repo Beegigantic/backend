124a3f8ec47a676e6f1d58170afe546011e58c2c
Beegigantic <melanie.c.janzen@gmail.com>
2025-11-25 13:03:50 -0800

    Update python.py

--- backend/python.py
+++ backend/python.py
@@ -1,10 +1,10 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def home():
-    return open("index.html").read()
+    return render_template("index.html")
 
 @app.route("/login", methods=["POST"])
 def login():
