c4e697f20c3e0007d71c2e97d294e94d19c685eb
Beegigantic <melanie.c.janzen@gmail.com>
2025-11-25 12:48:45 -0800

    Update python.py

--- backend/python.py
+++ backend/python.py
@@ -1,1 +1,21 @@
+from flask import Flask, request
 
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def home():
+    return open("index.html").read()
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    print("Received login:")
+    print("Name:", username)
+    print("Password:", password)
+
+    return "Access granted"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
